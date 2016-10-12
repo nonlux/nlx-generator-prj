80497b6ea2ea4852101e87cad78a518843659fab
nonlux <nonluxi@gmail.com>
2016-10-12 20:05:38 +0300

    es6  js project

--- generators/js/index.js
+++ generators/js/index.js
@@ -6,21 +6,48 @@ var path = require('path');
 
 module.exports = yeoman.Base.extend({
 
-  initializing: function() {
-  },
+    initializing: function() {
+    },
 
-  writing: function () {
-      if (this.config.get('projectType') === 'javascript') {
-          this.spawnCommandSync('npm', ['init', '-f']);
-          var config = {
-            version: "0.0.1",
+    writing: function () {
+        var name = this.config.get('projectName');
+        var config = {
             license:  "MIT",
             author: "nonlux <nonluxi@gmail.com>",
-          };
-          this.fs.extendJSON(this.destinationPath('package.json'), config);
-      }
-  },
+            scripts: {
+                postinstall: './npm-post.sh',
+            }
+        };
+        if (this.config.get('isGithub')) {
+            var url = "https://github.com/nonlux/"+name;
+            config.repository= {
+                "type": "git",
+                "url": url+".git"
+            };
+            config.bugs= {
+                "url": url+"/issues"
+            };
+            config.homepage= url;
+        };
+        if (this.config.get('projectType') === 'javascript') {
+            if (!this.fs.exists( this.destinationPath('package.json') )) {
+                this.spawnCommandSync('npm', ['init', '-f']);
+                this.fs.extendJSON(this.destinationPath('package.json'), config);
+            }
+        }
+        this.fs.copyTpl(
+            this.templatePath('npm-post.sh'),
+            this.destinationPath('npm-post.sh'),
+            this.props
+        );
+        this.fs.write(
+            this.destinationPath('.gitignore'),
+            this.fs.read(this.destinationPath('.gitignore'))+
+            this.fs.read(this.templatePath('.gitignore')),
+        );
 
-  install: function () {
-  }
+    },
+    install: function () {
+        this.spawnCommand('npm', ['install', 'nlx-babel-config', '--save-dev']);
+    }
 });
